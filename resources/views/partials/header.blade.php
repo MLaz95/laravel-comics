e80ac07f4c8e5d8fc7cb6cc992566e5d653e0e69
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-04-08 15:42:13 +0200

    aggiunta placeholder navbar

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<div id="top-bar">
+    <ul class="container d-flex justify-content-end gap-3 text-white">
+        <li>test</li>
+        <li>test</li>
+    </ul>
+</div>
+<nav class="navbar bg-white">
+    <div class="container">
+        <img class="nav-logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
