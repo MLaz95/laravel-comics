b7931cc4f8b1a031df9bbae4270072149accfba6
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-04-08 17:22:16 +0200

    aggiunto stile main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts/app')
 
 @section('content')
-    <main>
-        <div class="container">
+    <main class="pb-3">
+
+        <img id="jumbotron" src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+
+        <div class="container d-flex flex-column align-items-center">
+            <div class="text-white text-uppercase my_page_tag px-3 py-2 align-self-start">
+                <h2 class="m-0">
+                    Current series
+                </h2>
+            </div>
             <div class="row row-cols-6 row-gap-3">
                 @foreach ($comics as $singleComic)
                 <div class="col">
-                    <div class="card h-100 border-0">
+                    <div class="card h-100 border-0 rounded-0 bg-transparent text-white">
                         <div class="ratio ratio-1x1">
                             <img src="{{$singleComic['thumb']}}" class="object-fit-cover cover" alt="{{$singleComic['title']}}">
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">{{$singleComic['title']}}</h5>
+                        <div class="py-4">
+                            <h5 class="text-uppercase p-0">{{$singleComic['title']}}</h5>
                         </div>
                     </div>
                 </div>
                 @endforeach
             </div>
+            <button class="border-0 text-white text-uppercase fw-bold py-2 px-5">Load More</button>
         </div>
     </main>
 @endsection
